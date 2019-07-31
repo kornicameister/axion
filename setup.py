c2fb798dbe5939c5f7220a58f01bf07f9c5a4f76
Tomasz Trębski <kornicameister@gmail.com>
2019-08-01 00:23:00 +0200

    Ignore mypy report (#8)

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import sys
 
 import setuptools
 
+__version__ = '0.0.0'
 __title__ = 'axion'
 __author__ = 'Tomasz Trębski'
 __author_email__ = 'kornicameister@gmail.com'
@@ -9,7 +10,7 @@ __maintainer__ = __author__
 __url__ = f'https://github.com/kornicameister/{__title__}'
 
 if sys.version_info < (3, 6):
-    raise RuntimeError(f'{__title__} requires Python 3.6 or greater')
+    raise RuntimeError(f'{__title__}:{__version__} requires Python 3.6 or greater')
 
 setuptools.setup(
     setup_requires='setupmeta',
