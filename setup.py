dc18c42505309a7e7a773afff359c00388bca791
Tomasz Trębski <kornicameister@gmail.com>
2019-08-02 09:43:33 +0200

    Fix __url__ (#20)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __title__ = 'axion'
 __author__ = 'Tomasz Trębski'
 __author_email__ = 'kornicameister@gmail.com'
 __maintainer__ = __author__
-__url__ = f'https://github.com/kornicameister/{__title__}'
+__url__ = 'https://github.com/kornicameister/axion'
 
 if sys.version_info < (3, 6):
     raise RuntimeError(f'{__title__}:{__version__} requires Python 3.6 or greater')
