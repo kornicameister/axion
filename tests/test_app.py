f3dbac5a2bc88cef1c79254a431e523019e1f8ed
Tomasz Trębski <kornicameister@gmail.com>
2019-08-02 23:13:45 +0200

    Add test for Application (#21)

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import pytest_mock as ptm
+
+from axion import app
+
+
+def test_app_init(mocker: ptm.MockFixture, tmp_path: Path) -> None:
+    loaded_spec = mocker.stub()
+    spec_load = mocker.patch('axion.spec.load', return_value=loaded_spec)
+    spec_location = tmp_path / 'openapi.yaml'
+
+    the_app = app.Application(spec_location=spec_location)
+
+    spec_load.assert_called_once_with(spec_location)
+    assert the_app.spec == loaded_spec
