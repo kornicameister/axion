2cc91133dd2a4c35db8264016a6d8385bc473a67
Tomasz Trębski <kornicameister@gmail.com>
2019-08-19 00:30:44 +0200

    Analyze handler signature

--- axion/application/handler.py
+++ axion/application/handler.py
@@ -1,15 +1,32 @@
 import asyncio
 import importlib
+import inspect
 import typing as t
 
 from loguru import logger
+
+from axion import specification
+
+Handler = t.Callable[..., t.Awaitable[t.Any]]
 
 
 class InvalidHandlerError(Exception):
     ...
 
 
-def resolve(operation_id: str) -> t.Callable[..., t.Awaitable[t.Any]]:
+class OASHandlerMismatch(Exception):
+    ...
+
+
+def make(operation: specification.OASOperation) -> Handler:
+    logger.info('Making user handler for op={op}', op=operation)
+    return _analyze(
+        _resolve(operation.operation_id),
+        operation,
+    )
+
+
+def _resolve(operation_id: str) -> Handler:
     logger.opt(lazy=True).debug(
         'Resolving user handler via operation_id={operation_id}',
         operation_id=lambda: operation_id,
@@ -29,4 +46,27 @@ def resolve(operation_id: str) -> t.Callable[..., t.Awaitable[t.Any]]:
             f'Failed to locate function={function_name} in module={module_name}',
         ) from err
     else:
-        return t.cast(t.Callable[..., t.Awaitable[t.Any]], function)
+        return t.cast(Handler, function)
+
+
+def _analyze(
+        handler: Handler,
+        operation: specification.OASOperation,
+) -> Handler:
+    signature = inspect.signature(handler)
+
+    for op_param in operation.parameters:
+        try:
+            handler_param = signature.parameters[op_param.name]
+            if handler_param.annotation != op_param.python_type:
+                raise OASHandlerMismatch(
+                    f'{op_param.name} argument of {handler} expected to have '
+                    f'type {op_param.python_type}, but in fact it is '
+                    f'{handler_param.annotation}',
+                )
+        except KeyError as err:
+            raise OASHandlerMismatch(
+                f'{op_param} not found in {handler} arguments',
+            ) from err
+
+    return handler
--- tests/test_application_handler.py
+++ tests/test_application_handler.py
@@ -1,6 +1,10 @@
-import pytest
+import typing as t
 
-from axion import application
+import pytest
+import pytest_mock as ptm
+
+from axion.application import handler
+from axion.specification import parser
 
 
 def normal_f() -> None:
@@ -12,27 +16,83 @@ async def async_f() -> None:
 
 
 def test_resolve_handler_module_not_found() -> None:
-    with pytest.raises(application.InvalidHandlerError) as err:
-        application.resolve_handler('really_dummy.api.get_all')
+    with pytest.raises(handler.InvalidHandlerError) as err:
+        handler._resolve('really_dummy.api.get_all')
     assert err.match('Failed to import module=really_dummy.api')
 
 
 def test_resolve_handler_function_not_found() -> None:
-    with pytest.raises(application.InvalidHandlerError) as err:
-        application.resolve_handler('tests.test_application_handler.foo')
+    with pytest.raises(handler.InvalidHandlerError) as err:
+        handler._resolve('tests.test_application_handler.foo')
     assert err.match(
         'Failed to locate function=foo in module=tests.test_application_handler',
     )
 
 
 def test_resolve_handler_not_couroutine() -> None:
-    with pytest.raises(application.InvalidHandlerError) as err:
-        application.resolve_handler('tests.test_application_handler.normal_f')
+    with pytest.raises(handler.InvalidHandlerError) as err:
+        handler._resolve('tests.test_application_handler.normal_f')
     assert err.match(
         'tests.test_application_handler.normal_f did not resolve to coroutine',
     )
 
 
 def test_resolve_handler_couroutine() -> None:
-    handler = application.resolve_handler('tests.test_application_handler.async_f')
-    assert handler is async_f
+    assert handler._resolve('tests.test_application_handler.async_f') is async_f
+
+
+def test_analyze_handler(mocker: ptm.MockFixture) -> None:
+    async def get_one(
+            id: str,
+            limit: t.Optional[int],
+            include_extra: t.Optional[bool],
+    ) -> None:
+        ...
+
+    operation = list(
+        parser._resolve_operations(
+            components={},
+            paths={
+                '/{name}': {
+                    'post': {
+                        'operationId': 'foo',
+                        'responses': {
+                            'default': {
+                                'description': 'fake',
+                            },
+                        },
+                        'parameters': [
+                            {
+                                'name': 'id',
+                                'in': 'path',
+                                'required': True,
+                                'schema': {
+                                    'type': 'string',
+                                },
+                            },
+                            {
+                                'name': 'limit',
+                                'in': 'query',
+                                'schema': {
+                                    'type': 'integer',
+                                },
+                            },
+                            {
+                                'name': 'includeExtra',
+                                'in': 'query',
+                                'schema': {
+                                    'type': 'boolean',
+                                    'default': True,
+                                },
+                            },
+                        ],
+                    },
+                },
+            },
+        ),
+    )[0]
+
+    handler._analyze(
+        handler=get_one,
+        operation=operation,
+    )
