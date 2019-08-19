1bf66d1e757903605aa3efffa5ea22341968ed8b
Tomasz Trębski <kornicameister@gmail.com>
2019-08-19 23:07:24 +0200

    Increase coverage (#51)

--- tests/test_spec_oas_string.py
+++ tests/test_spec_oas_string.py
@@ -3,6 +3,7 @@ import typing as t
 import pytest
 
 from axion.specification import exceptions
+from axion.specification import model
 from axion.specification import parser
 
 
@@ -28,3 +29,24 @@ def test_spec_build_oas_string_invalid_min_max_length(
             'minLength': min_length,
             'maxLength': max_length,
         })
+
+
+@pytest.mark.parametrize(
+    'pattern,should_match,should_not_match',
+    (
+        ('^[a-z]*$', ['a', 'ab', 'abc'], ['123', '456']),
+        ('^[a-z0-9_-]{3,16}$', ['my-us3r_n4m3'], ['th1s1s-wayt00_l0ngt0beausername']),
+    ),
+)
+def test_spec_build_oas_number_pattern(
+        pattern: str,
+        should_match: t.List[str],
+        should_not_match: t.List[str],
+) -> None:
+    oas_string = parser._build_oas_string({'pattern': pattern})
+    assert isinstance(oas_string, model.OASStringType)
+    assert oas_string.pattern is not None
+    for example in should_match:
+        assert oas_string.pattern.match(example) is not None, f'{pattern} did not match'
+    for example in should_not_match:
+        assert oas_string.pattern.match(example) is None, f'{pattern} did match'
