92837e8eddd7b68c0878ebf9426630f2d46ef3c5
Tomasz Trębski <kornicameister@gmail.com>
2020-01-02 00:49:24 +0100

    Extend typing_inspect usage (#121)

--- tests/test_application_handler.py
+++ tests/test_application_handler.py
@@ -9,6 +9,117 @@ from axion.specification import model
 from axion.specification import parser
 
 
+@pytest.mark.parametrize(
+    'the_type,str_repr',
+    (
+        (str, 'str'),
+        (bool, 'bool'),
+        (float, 'float'),
+        (list, 'list'),
+        (dict, 'dict'),
+        (set, 'set'),
+        (t.NewType('Cookies', int), 'Cookies[int]'),
+        (t.NewType('Cookies', bool), 'Cookies[bool]'),
+        (t.NewType('Cookies', str), 'Cookies[str]'),
+        (t.NewType('Cookies', float), 'Cookies[float]'),
+        (t.NewType('Cookies', t.List[str]), 'Cookies[typing.List[str]]'),
+        (t.NewType('Cookies', t.Set[str]), 'Cookies[typing.Set[str]]'),
+        (t.TypeVar('T'), 'typing.Any'),  # type: ignore
+        (t.TypeVar('T', int, float), 'typing.TypeVar(?, int, float)'),  # type: ignore
+        (t.TypeVar('T', int, float,  # type: ignore
+                   bool), 'typing.TypeVar(?, int, float, bool)'),
+        (t.TypeVar('T', bound=list), 'list'),  # type: ignore
+        (t.TypeVar('T', bound=set), 'set'),  # type: ignore
+        (t.AnyStr, 'typing.TypeVar(?, bytes, str)'),
+        (t.Union[int, float], 'typing.Union[int, float]'),
+        (t.Union[int], 'int'),
+        (t.Optional[int], 'typing.Optional[int]'),
+        (t.Optional[float], 'typing.Optional[float]'),
+        (t.Optional[bool], 'typing.Optional[bool]'),
+        (
+            t.Optional[t.Union[float, int]],
+            'typing.Optional[typing.Union[float, int]]',
+        ),
+        (
+            t.Optional[t.Union[int, float]],
+            'typing.Optional[typing.Union[float, int]]',
+        ),
+        (
+            t.Optional[t.Union[dict, set, list]],
+            'typing.Optional[typing.Union[dict, set, list]]',
+        ),
+        (
+            t.Optional[t.Union[t.AnyStr, int, float]],
+            'typing.Optional[typing.Union[typing.TypeVar(?, bytes, str), int, float]]',
+        ),
+        (t.Dict[str, str], 'typing.Dict[str, str]'),
+        (t.Optional[t.Dict[str, str]], 'typing.Optional[typing.Dict[str, str]]'),
+        (t.Optional[t.Dict[str, t.Any]], 'typing.Optional[typing.Dict[str, typing.Any]]'),
+        (t.Dict, 'typing.Dict[typing.Any, typing.Any]'),
+        (t.Dict[t.Any, t.Any], 'typing.Dict[typing.Any, typing.Any]'),
+        (t.Set, 'typing.Set[typing.Any]'),
+        (t.Set[str], 'typing.Set[str]'),
+        (t.Set[bool], 'typing.Set[bool]'),
+        (t.Set[float], 'typing.Set[float]'),
+        (t.Set[t.Any], 'typing.Set[typing.Any]'),
+        (t.Mapping, 'typing.Mapping[typing.Any, typing.Any]'),
+        (t.Mapping[str, str], 'typing.Mapping[str, str]'),
+        (t.Mapping[str, int], 'typing.Mapping[str, int]'),
+        (t.Mapping[int, str], 'typing.Mapping[int, str]'),
+        (t.AbstractSet, 'typing.AbstractSet[typing.Any]'),
+        (t.AbstractSet[bool], 'typing.AbstractSet[bool]'),
+        (t.Optional[t.AbstractSet[bool]], 'typing.Optional[typing.AbstractSet[bool]]'),
+        (None, None),
+        (t.Any, 'typing.Any'),
+        (te.TypedDict, 'typing_extensions.TypedDict'),
+        (
+            te.TypedDict(  # type: ignore
+                'Cookies',
+                {
+                    'debug': bool,
+                    'csrftoken': str,
+                },
+            ),
+            'Cookies{debug: bool, csrftoken: str}',
+        ),
+        (
+            te.TypedDict(  # type: ignore
+                'Paging',
+                {
+                    'page': t.Optional[int],
+                    'hasNext': bool,
+                    'hasPrev': bool,
+                },
+            ),
+            'Paging{page: typing.Optional[int], hasNext: bool, hasPrev: bool}',
+        ),
+        (
+            te.TypedDict(  # type: ignore
+                'Complex',
+                {
+                    'page': t.Optional[int],
+                    'foo': t.Union[t.List[str], t.Set[float]],
+                    'bar': te.TypedDict('Bar', {'little': bool}),  # type: ignore
+                },
+            ),
+            (
+                'Complex{'
+                'page: typing.Optional[int], '
+                'foo: typing.Union[typing.List[str], typing.Set[float]],'
+                ' bar: Bar{little: bool}'
+                '}'
+            ),
+        ),
+    ),
+)
+def test_get_type_string_repr(the_type: t.Optional[t.Type[t.Any]], str_repr: str) -> None:
+    if the_type is None:
+        with pytest.raises(AssertionError):
+            handler._get_type_string_repr(the_type)  # type: ignore
+    else:
+        assert handler._get_type_string_repr(the_type) == str_repr
+
+
 def normal_f() -> None:
     ...
 
@@ -364,7 +475,7 @@ class TestCookies:
         assert len(err.value) == len(expected_errors)
         for param_key, error_msg in expected_errors:
             assert f'cookies.{param_key}' in err.value
-            assert repr(err.value[f'cookies.{param_key}']) == error_msg
+            assert err.value[f'cookies.{param_key}'] == error_msg
 
     @pytest.mark.parametrize(
         'the_type',
@@ -396,10 +507,10 @@ class TestCookies:
 
         assert len(err.value) == 1
         assert 'cookies' in err.value
-        assert repr(err.value['cookies']) == (
+        assert err.value['cookies'] == (
             f'expected [typing.Mapping[str, typing.Any],'
-            f'typing.Dict[str, typing.Any],TypedDict]'
-            f', but got {handler._readable_t(the_type)}'
+            f'typing.Dict[str, typing.Any],typing_extensions.TypedDict]'
+            f', but got {handler.get_type_string_repr(the_type)}'
         )
 
     @pytest.mark.parametrize(
@@ -685,7 +796,7 @@ class TestHeaders:
 
         assert len(err.value) == 1
         assert 'headers.accept' in err.value
-        assert repr(err.value['headers.accept']) == 'expected [str], but got int'
+        assert err.value['headers.accept'] == 'expected [str], but got int'
 
     def test_no_oas_headers_typed_dict(
             self,
@@ -888,9 +999,9 @@ class TestHeaders:
 
         assert len(err.value) == 1
         assert 'headers.x_trace_id' in err.value
-        assert repr(
-            err.value['headers.x_trace_id'],
-        ) == f'expected [str], but got {handler._readable_t(the_type)}'
+        assert (f'expected [str], '
+                f'but got {handler.get_type_string_repr(the_type)}'
+                ) == err.value['headers.x_trace_id']
 
 
 class TestPathQuery:
@@ -997,7 +1108,7 @@ class TestPathQuery:
         assert err.value.operation_id == 'TestAnalysisParameters'
         assert len(err.value) == 1
         assert 'id' in err.value
-        assert repr(err.value['id']) == 'expected [str], but got bool'
+        assert err.value['id'] == 'expected [str], but got bool'
 
     def test_signature_all_bad_type(self) -> None:
         async def foo(
@@ -1017,27 +1128,29 @@ class TestPathQuery:
         assert err.value.operation_id == 'TestAnalysisParameters'
         assert len(err.value) == 4
         for mismatch in err.value:
+            actual_msg = err.value[mismatch.param_name]
+            expected_msg = None
+
             if mismatch.param_name == 'id':
-                assert repr(
-                    err.value[mismatch.param_name],
-                ) == 'expected [str], but got float'
+                expected_msg = 'expected [str], but got float'
             elif mismatch.param_name == 'limit':
-                assert repr(
-                    err.value[mismatch.param_name],
-                ) == 'expected [typing.Optional[int]], but got typing.Optional[float,int]'
+                expected_msg = (
+                    'expected [typing.Optional[int]], but got '
+                    'typing.Optional[typing.Union[float, int]]'
+                )
             elif mismatch.param_name == 'page':
                 expected_msg = (
                     'expected [typing.Optional[float]], but got '
                     'typing.Optional[typing.AbstractSet[bool]]'
                 )
-                assert repr(err.value[mismatch.param_name]) == expected_msg
-            elif mismatch.param_name == 'include_extra':
-                assert repr(
-                    err.value[mismatch.param_name],
-                ) == (
+            elif mismatch.param_name == 'includeExtra':
+                expected_msg = (
                     'expected [typing.Optional[bool]], but got '
-                    'typing.Union[int,str]'
+                    'typing.Union[int, str]'
                 )
+
+            assert expected_msg is not None
+            assert actual_msg == expected_msg
 
     def test_signature_match(self) -> None:
         async def test_handler(
@@ -1116,13 +1229,12 @@ class TestBody:
         assert err.value
         assert 1 == len(err.value)
         assert 'body' in err.value
-        assert (
-            'expected ['
-            'typing.Mapping[str, typing.Any],'
-            'typing.Dict[str, typing.Any]], '
-            'but got '
-            'typing.Union[typing.Dict[str, typing.Any], NoneType]'
-        ) == repr(err.value['body'])
+        assert err.value['body'] == (
+            'expected '
+            '[typing.Mapping[str, typing.Any],typing.Dict[str, typing.Any]]'
+            ', but got '
+            'typing.Optional[typing.Dict[str, typing.Any]]'
+        )
 
     @pytest.mark.parametrize(
         'the_type',
