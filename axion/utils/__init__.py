61df40118748120b9796543a3b252d581d115ed9
Tomasz Trębski <kornicameister@gmail.com>
2020-01-02 23:38:55 +0100

    Move resolving type_string_repr to own module

---
+++ axion/utils/__init__.py
@@ -0,0 +1,5 @@
+from axion.utils import type_str
+
+type_str_repr = type_str.repr
+
+__all__ = ('type_str_repr', )
--- tests/test_application_handler.py
+++ tests/test_application_handler.py
@@ -4,120 +4,10 @@ from _pytest import logging
 import pytest
 import typing_extensions as te
 
+from axion import utils
 from axion.application import handler
 from axion.specification import model
 from axion.specification import parser
-
-
-@pytest.mark.parametrize(
-    'the_type,str_repr',
-    (
-        (str, 'str'),
-        (bool, 'bool'),
-        (float, 'float'),
-        (list, 'list'),
-        (dict, 'dict'),
-        (set, 'set'),
-        (t.NewType('Cookies', int), 'Cookies[int]'),
-        (t.NewType('Cookies', bool), 'Cookies[bool]'),
-        (t.NewType('Cookies', str), 'Cookies[str]'),
-        (t.NewType('Cookies', float), 'Cookies[float]'),
-        (t.NewType('Cookies', t.List[str]), 'Cookies[typing.List[str]]'),
-        (t.NewType('Cookies', t.Set[str]), 'Cookies[typing.Set[str]]'),
-        (t.TypeVar('T'), 'typing.Any'),  # type: ignore
-        (t.TypeVar('T', int, float), 'typing.TypeVar(?, int, float)'),  # type: ignore
-        (t.TypeVar('T', int, float,  # type: ignore
-                   bool), 'typing.TypeVar(?, int, float, bool)'),
-        (t.TypeVar('T', bound=list), 'list'),  # type: ignore
-        (t.TypeVar('T', bound=set), 'set'),  # type: ignore
-        (t.AnyStr, 'typing.TypeVar(?, bytes, str)'),
-        (t.Union[int, float], 'typing.Union[int, float]'),
-        (t.Union[int], 'int'),
-        (t.Optional[int], 'typing.Optional[int]'),
-        (t.Optional[float], 'typing.Optional[float]'),
-        (t.Optional[bool], 'typing.Optional[bool]'),
-        (
-            t.Optional[t.Union[float, int]],
-            'typing.Optional[typing.Union[float, int]]',
-        ),
-        (
-            t.Optional[t.Union[int, float]],
-            'typing.Optional[typing.Union[float, int]]',
-        ),
-        (
-            t.Optional[t.Union[dict, set, list]],
-            'typing.Optional[typing.Union[dict, set, list]]',
-        ),
-        (
-            t.Optional[t.Union[t.AnyStr, int, float]],
-            'typing.Optional[typing.Union[typing.TypeVar(?, bytes, str), int, float]]',
-        ),
-        (t.Dict[str, str], 'typing.Dict[str, str]'),
-        (t.Optional[t.Dict[str, str]], 'typing.Optional[typing.Dict[str, str]]'),
-        (t.Optional[t.Dict[str, t.Any]], 'typing.Optional[typing.Dict[str, typing.Any]]'),
-        (t.Dict, 'typing.Dict[typing.Any, typing.Any]'),
-        (t.Dict[t.Any, t.Any], 'typing.Dict[typing.Any, typing.Any]'),
-        (t.Set, 'typing.Set[typing.Any]'),
-        (t.Set[str], 'typing.Set[str]'),
-        (t.Set[bool], 'typing.Set[bool]'),
-        (t.Set[float], 'typing.Set[float]'),
-        (t.Set[t.Any], 'typing.Set[typing.Any]'),
-        (t.Mapping, 'typing.Mapping[typing.Any, typing.Any]'),
-        (t.Mapping[str, str], 'typing.Mapping[str, str]'),
-        (t.Mapping[str, int], 'typing.Mapping[str, int]'),
-        (t.Mapping[int, str], 'typing.Mapping[int, str]'),
-        (t.AbstractSet, 'typing.AbstractSet[typing.Any]'),
-        (t.AbstractSet[bool], 'typing.AbstractSet[bool]'),
-        (t.Optional[t.AbstractSet[bool]], 'typing.Optional[typing.AbstractSet[bool]]'),
-        (None, None),
-        (t.Any, 'typing.Any'),
-        (te.TypedDict, 'typing_extensions.TypedDict'),
-        (
-            te.TypedDict(  # type: ignore
-                'Cookies',
-                {
-                    'debug': bool,
-                    'csrftoken': str,
-                },
-            ),
-            'Cookies{debug: bool, csrftoken: str}',
-        ),
-        (
-            te.TypedDict(  # type: ignore
-                'Paging',
-                {
-                    'page': t.Optional[int],
-                    'hasNext': bool,
-                    'hasPrev': bool,
-                },
-            ),
-            'Paging{page: typing.Optional[int], hasNext: bool, hasPrev: bool}',
-        ),
-        (
-            te.TypedDict(  # type: ignore
-                'Complex',
-                {
-                    'page': t.Optional[int],
-                    'foo': t.Union[t.List[str], t.Set[float]],
-                    'bar': te.TypedDict('Bar', {'little': bool}),  # type: ignore
-                },
-            ),
-            (
-                'Complex{'
-                'page: typing.Optional[int], '
-                'foo: typing.Union[typing.List[str], typing.Set[float]],'
-                ' bar: Bar{little: bool}'
-                '}'
-            ),
-        ),
-    ),
-)
-def test_get_type_string_repr(the_type: t.Optional[t.Type[t.Any]], str_repr: str) -> None:
-    if the_type is None:
-        with pytest.raises(AssertionError):
-            handler._get_type_string_repr(the_type)  # type: ignore
-    else:
-        assert handler._get_type_string_repr(the_type) == str_repr
 
 
 def normal_f() -> None:
@@ -510,7 +400,7 @@ class TestCookies:
         assert err.value['cookies'] == (
             f'expected [typing.Mapping[str, typing.Any],'
             f'typing.Dict[str, typing.Any],typing_extensions.TypedDict]'
-            f', but got {handler.get_type_string_repr(the_type)}'
+            f', but got {utils.type_str_repr(the_type)}'
         )
 
     @pytest.mark.parametrize(
@@ -1000,7 +890,7 @@ class TestHeaders:
         assert len(err.value) == 1
         assert 'headers.x_trace_id' in err.value
         assert (f'expected [str], '
-                f'but got {handler.get_type_string_repr(the_type)}'
+                f'but got {utils.type_str_repr(the_type)}'
                 ) == err.value['headers.x_trace_id']
 
 
