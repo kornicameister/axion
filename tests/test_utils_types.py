641cf3ef81cdfb040dd70c7072ed4233550c85e3
Tomasz Trębski <kornicameister@gmail.com>
2020-01-11 00:09:37 +0100

    `is_any_type` + `is_dict_like` (#177)

--- tests/test_utils_types.py
+++ tests/test_utils_types.py
@@ -1,9 +1,75 @@
+import types
 import typing as t
 
 import pytest
 import typing_extensions as te
 
-from axion.utils import types
+from axion.utils import types as axion_types
+
+
+@pytest.mark.parametrize(
+    'the_type,expected_result',
+    (
+        *[(pp, False) for pp in axion_types.P_TYPES],
+        *[(t.NewType(repr(pp), pp), False) for pp in axion_types.P_TYPES],
+        *[
+            (t.Any, True),
+            (t.NewType('X', t.Any), True),  # type: ignore
+        ],
+    ),
+)
+def test_is_any_type(the_type: t.Any, expected_result: bool) -> None:
+    actual_result = axion_types.is_any_type(the_type)
+    assert expected_result == actual_result
+
+
+@pytest.mark.parametrize('the_type', axion_types.P_TYPES)
+def test_is_not_any_type(the_type: t.Any) -> None:
+    assert not axion_types.is_any_type(the_type)
+
+
+@pytest.mark.parametrize(
+    'the_type,expected_result',
+    (
+        (t.Mapping[str, str], True),
+        (t.Dict[str, str], True),
+        (t.Mapping[int, float], True),
+        (te.TypedDict('Z', z=int), True),  # type: ignore
+        (t.List[int], False),
+        (int, False),
+        (bool, False),
+        (complex, False),
+        (t.Set[int], False),
+        (t.Dict[str, complex], True),
+        (
+            types.new_class('C1', (t.Dict[str, str], )),
+            True,
+        ),
+        (
+            types.new_class('C2', (t.Dict[t.Any, complex], )),
+            True,
+        ),
+        (
+            types.new_class('C3', (t.Mapping[str, float], )),
+            True,
+        ),
+        (
+            types.new_class('C3', (t.NamedTuple, )),
+            False,
+        ),
+        (
+            t.NewType('C4', t.Dict[str, t.Set[complex]]),
+            True,
+        ),
+        (
+            t.NewType('C5', int),
+            False,
+        ),
+    ),
+)
+def test_is_dict_like(the_type: t.Any, expected_result: bool) -> None:
+    actual_result = axion_types.is_dict_like(the_type)
+    assert expected_result == actual_result
 
 
 @pytest.mark.parametrize(
@@ -19,7 +85,7 @@ from axion.utils import types
     ),
 )
 def test_is_new_type(the_type: t.Any, expected_result: bool) -> None:
-    actual_result = types.is_new_type(the_type)
+    actual_result = axion_types.is_new_type(the_type)
     assert expected_result == actual_result
 
 
@@ -34,7 +100,7 @@ def test_is_new_type(the_type: t.Any, expected_result: bool) -> None:
     ),
 )
 def test_is_none_type(the_type: t.Any, expected_result: bool) -> None:
-    actual_result = types.is_none_type(the_type)
+    actual_result = axion_types.is_none_type(the_type)
     assert expected_result == actual_result
 
 
@@ -71,13 +137,13 @@ def test_literal_types(
         the_type: t.Any,
         expected_types: t.Sequence[t.Any],
 ) -> None:
-    actual_types = types.literal_types(the_type)
+    actual_types = axion_types.literal_types(the_type)
     assert expected_types == actual_types
 
 
 def test_literal_types_not_literal_input() -> None:
     with pytest.raises(AssertionError):
-        types.literal_types(t.Mapping[str, str])
+        axion_types.literal_types(t.Mapping[str, str])
 
 
 @pytest.mark.parametrize(
@@ -90,4 +156,4 @@ def test_literal_types_not_literal_input() -> None:
 )
 def test_literal_types_not_resolvable_type(the_type: t.Any) -> None:
     with pytest.raises(TypeError):
-        types.literal_types(the_type)
+        axion_types.literal_types(the_type)
