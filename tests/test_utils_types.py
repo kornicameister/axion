9c93fa8997e0c80b7cc04543727d3a21b35413e7
Tomasz Trębski <kornicameister@gmail.com>
2020-01-16 21:34:58 +0100

    fixup! Add 2 test cases for is_none_type

--- tests/test_utils_types.py
+++ tests/test_utils_types.py
@@ -100,7 +100,7 @@ def test_is_new_type(the_type: t.Any, expected_result: bool) -> None:
         (type(None), True),
         (int, False),
         (te.Literal[None], False),
-        (te.Literal[None, 204], False),        
+        (te.Literal[None, 204], False),
         (t.Dict[str, str], False),
         (t.NamedTuple('NT', x=int), False),
     ),
