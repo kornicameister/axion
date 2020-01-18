ab1881bd807dfe1599a12ee643b0c3fe894c014b
Tomasz Trębski <kornicameister@gmail.com>
2020-01-18 23:38:57 +0100

    fixup! Fix loading builtin plugins

--- tests/test_axion.py
+++ tests/test_axion.py
@@ -3,6 +3,5 @@ import axion
 
 def test_builtin_plugins_detected() -> None:
     assert len(axion._plugins()) == 1
-    assert len(axion._plugins()) == 1
-    assert len(axion._plugins()) == 1
-    assert len(axion._plugins()) == 1
+    assert 'aiohttp' in axion._plugins()
+    assert 1 == len(getattr(axion._plugins, '__cache__', []))
