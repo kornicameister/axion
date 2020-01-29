50e440850a914116cd27acf10d7864b5e887a09e
Tomasz Trebski <tomasz.trebski@ts.fujitsu.com>
2020-01-29 08:21:20 +0100

    Copy changes from #228

--- examples/petstore/api/__init__.py
+++ examples/petstore/api/__init__.py
@@ -1,10 +1,12 @@
 import typing as t
 
+from axion import oas_endpoint
 from axion import response
 
 
+@oas_endpoint
 async def list_pets(
-        tags: t.Optional[list] = None,
+        tags: t.Optional[t.List[str]] = None,
         limit: t.Optional[int] = None,
 ) -> response.Response:
     return {
@@ -12,18 +14,21 @@ async def list_pets(
     }
 
 
+@oas_endpoint
 async def find_pet(id: int) -> response.Response:
     return {
         'http_code': 200,
     }
 
 
+@oas_endpoint
 async def delete_pet(id: int) -> response.Response:
     return {
         'http_code': 204,
     }
 
 
+@oas_endpoint
 async def new_pet(body: t.Mapping[str, str]) -> response.Response:
     return {
         'http_code': 200,
