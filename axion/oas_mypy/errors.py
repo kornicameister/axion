6d08793e8ff6f360c06a96379e403ca27b8725b7
Tomasz Trębski <kornicameister@gmail.com>
2020-02-24 08:05:11 +0100

    [TYPING] Tests for cases where handler is not in fact part of OAS (#265)
    Partially: #189

--- axion/oas_mypy/errors.py
+++ axion/oas_mypy/errors.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
 from mypy.errorcodes import ErrorCode
+from mypy.plugin import FunctionContext
+from mypy.types import Type
 from typing_extensions import Final
 
 ERROR_UNKNOWN_PLUGIN: Final[ErrorCode] = ErrorCode(
@@ -21,3 +25,21 @@ ERROR_INVALID_OAS_VALUE: Final[ErrorCode] = ErrorCode(
     'Handler argument (default) value does not conform to OAS specification',
     'OAS',
 )
+
+
+def not_oas_handler(
+        msg: str,
+        code: ErrorCode,
+        ctx: FunctionContext,
+        line_number: Optional[int] = None,
+) -> Type:
+    context = ctx.context
+    context.line = line_number or context.line
+
+    ctx.api.msg.fail(
+        msg,
+        context=context,
+        code=code,
+    )
+
+    return ctx.default_return_type
