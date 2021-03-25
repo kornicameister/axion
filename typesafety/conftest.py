f3d8056d82d1d53dd9f2e4ee0dac1f8923a63280
Tomasz Trębski <tomasz.trebski@fujitsu.com>
2021-03-25 21:28:23 +0100

    Silence oas validator logs in case of failure

--- typesafety/conftest.py
+++ typesafety/conftest.py
@@ -1,10 +1,11 @@
+import logging
+
 import pytest
 
+logging.getLogger('openapi_spec_validator').setLevel(logging.ERROR)
 
-@pytest.fixture(
-    scope='session',
-    autouse=True,
-)
+
+@pytest.fixture(scope='session', autouse=True)
 def axion_plugins() -> None:
     from axion import _plugins
 
